fe904977535b40bfb662afa4b140cd1db797e6bc
GrigoryevAO <agrigoryev13@yandex.ru>
2025-02-19 12:41:32 +0300

    init: Initial commit

---
+++ src/main/java/org/example/tokenlens/TokenLensApplication.java
@@ -0,0 +1,15 @@
+package org.example.tokenlens;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class TokenLensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokenLensApplication.class, args);
+    }
+
+}
